a12afd42d46eee8a38045e2277afaabbc3ac8c15
Olapade1-code <olapademoses7@gmail.com>
2025-03-04 15:11:28 +0100

    Update tailwind.config.js

--- lesson/tailwind.config.js
+++ lesson/tailwind.config.js
@@ -1,24 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-module.export = {
-    content: ["./lesson/build/**/*.{html,js}"],/* './lesson/build/js/*.main.js' */],
+module.exports = {
+    content: ["./lesson/build/**/*.{html,js}"],
     theme: {
         extend: {
             screens: {
-            'widescreen': { 'raw': '(min-aspect-ratio: 3/2)'},
-            'tallscreen': { 'raw': '(min-aspect-ratio: 13/20)'},
+                'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
+                'tallscreen': { 'raw': '(min-aspect-ratio: 13/20)' },
             },
-keyframes: {
-    'open-menu':{
-        '0%': { transform: 'scaleY(0)'},
-        '80%': { transform: 'scaleY(1,2)'},
-        '100%': { transform: 'scaleY(1)'},
-    }
-},
-    animation: {
-        'open-menu': 'open-menu 0.5s ease-in-out forwards',
-    }
-
-        }, 
+            keyframes: {
+                'open-menu': {
+                    '0%': { transform: 'scaleY(0)' },
+                    '80%': { transform: 'scaleY(1.2)' },
+                    '100%': { transform: 'scaleY(1)' },
+                }
+            },
+            animation: {
+                'open-menu': 'open-menu 0.5s ease-in-out forwards',
+            }
+        },
     },
-    plugins: []
-},
+    plugins: [],
+};
